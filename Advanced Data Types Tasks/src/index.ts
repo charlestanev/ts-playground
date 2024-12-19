b6b1e40394613a9377c0eb0fe66b8ae759f85bf2
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2024-12-19 15:20:26 +0200

    Implement town processing function to parse and format town data from input strings

--- Advanced Data Types Tasks/src/index.ts
+++ Advanced Data Types Tasks/src/index.ts
@@ -69,83 +69,137 @@
 
 
 
-// 3.	Inventory
-// Create a function, which creates a register for heroes, with their names, level, and items (if they have such).
-// The input comes as an array of strings. Each element holds data for a hero, in the following format:
-// "{heroName} / {heroLevel} / {item1}, {item2}, {item3}..."
-// You must store the data about every hero. The name is a string, a level is a number and the items are all strings.
-// The output is all of the data for all the heroes you’ve stored sorted ascending by level. The data must be in the following format for each hero:
-// Hero: {heroName}
-// level => {heroLevel}
-// Items => {item1}, {item2}, {item3}
+// // 3.	Inventory
+// // Create a function, which creates a register for heroes, with their names, level, and items (if they have such).
+// // The input comes as an array of strings. Each element holds data for a hero, in the following format:
+// // "{heroName} / {heroLevel} / {item1}, {item2}, {item3}..."
+// // You must store the data about every hero. The name is a string, a level is a number and the items are all strings.
+// // The output is all of the data for all the heroes you’ve stored sorted ascending by level. The data must be in the following format for each hero:
+// // Hero: {heroName}
+// // level => {heroLevel}
+// // Items => {item1}, {item2}, {item3}
+// // Examples
+// // Input	Output
+// // [
+// // 'Isacc / 25 / Apple, GravityGun',
+// // 'Derek / 12 / BarrelVest, DestructionSword',
+// // 'Hes / 1 / Desolator, Sentinel, Antara'
+// // ]	Hero: Hes
+// // level => 1
+// // items => Desolator, Sentinel, Antara
+// // Hero: Derek
+// // level => 12
+// // items => BarrelVest, DestructionSword
+// // Hero: Isacc
+// // level => 25
+// // items => Apple, GravityGun
+// // [
+// // 'Batman / 2 / Banana, Gun',
+// // 'Superman / 18 / Sword',
+// // 'Poppy / 28 / Sentinel, Antara'
+// // ]	Hero: Batman
+// // level => 2
+// // items => Banana, Gun
+// // Hero: Superman
+// // level => 18
+// // items => Sword
+// // Hero: Poppy
+// // level => 28
+// // items => Sentinel, Antara
+
+// interface Hero {
+//     name: string;
+//     level: number;
+//     items: string[];
+// }
+
+// function registerHeroes(input: string[]): void {
+//     const heroes: Hero[] = [];
+
+//     input.forEach(x => {
+//         const [name, level, items] = x.split(' / ');
+
+//         heroes.push({
+//             name,
+//             level: Number(level),
+//             items: items ? items.split(', ') : []
+//         })
+//     })
+
+//     heroes.sort((a, b) => a.level - b.level);
+
+//     heroes.forEach(hero => {
+//         console.log(`Hero: ${hero.name}`);
+//         console.log(`level => ${hero.level}`);
+//         console.log(`items => ${hero.items.join(', ')}`);
+//     })
+// }
+
+// const input1 = [
+//     'Isacc / 25 / Apple, GravityGun',
+//     'Derek / 12 / BarrelVest, DestructionSword',
+//     'Hes / 1 / Desolator, Sentinel, Antara'
+// ];
+
+// const input2 = [
+//     'Batman / 2 / Banana, Gun',
+//     'Superman / 18 / Sword',
+//     'Poppy / 28 / Sentinel, Antara'
+// ];
+
+// registerHeroes(input1);
+// registerHeroes(input2);
+
+
+
+
+
+// 4.	Towns
+// You’re tasked to create and print objects from a text table. 
+// You will receive the input as an array of strings, where each string represents a table row, with values on the row separated by pipes " | " and spaces.
+// The table will consist of exactly 3 columns "Town", "Latitude" and "Longitude". The latitude and longitude columns will always contain valid numbers. Check the examples to get a better understanding of your task.
+// The output should be objects. Latitude and longitude must be parsed to numbers and formatted to the second decimal point!
 // Examples
-// Input	Output
-// [
-// 'Isacc / 25 / Apple, GravityGun',
-// 'Derek / 12 / BarrelVest, DestructionSword',
-// 'Hes / 1 / Desolator, Sentinel, Antara'
-// ]	Hero: Hes
-// level => 1
-// items => Desolator, Sentinel, Antara
-// Hero: Derek
-// level => 12
-// items => BarrelVest, DestructionSword
-// Hero: Isacc
-// level => 25
-// items => Apple, GravityGun
-// [
-// 'Batman / 2 / Banana, Gun',
-// 'Superman / 18 / Sword',
-// 'Poppy / 28 / Sentinel, Antara'
-// ]	Hero: Batman
-// level => 2
-// items => Banana, Gun
-// Hero: Superman
-// level => 18
-// items => Sword
-// Hero: Poppy
-// level => 28
-// items => Sentinel, Antara
+// Input
+// ['Sofia | 42.696552 | 23.32601',
+// 'Beijing | 39.913818 | 116.363625']
+// Output
+// { town: 'Sofia', latitude: '42.70', longitude: '23.33' }
+// { town: 'Beijing', latitude: '39.91', longitude: '116.36' }
 
-interface Hero {
-    name: string;
-    level: number;
-    items: string[];
+// Input
+// ['Plovdiv | 136.45 | 812.575']
+// Output
+// { town: 'Plovdiv', latitude: '136.45', longitude: '812.58' }
+
+interface Town {
+    town: string;
+    latitude: string;
+    longitude: string;
 }
 
-function registerHeroes(input: string[]): void {
-    const heroes: Hero[] = [];
+function processTowns(input: string[]): void {
+    input.forEach(row => {
+        const [town, latitude, longitude] = row.split(' | ');
 
-    input.forEach(x => {
-        const [name, level, items] = x.split(' / ');
+        const townObject: Town = {
+            town: town,
+            latitude: Number(latitude).toFixed(2),
+            longitude: Number(longitude).toFixed(2)
+        }
 
-        heroes.push({
-            name,
-            level: Number(level),
-            items: items ? items.split(', ') : []
-        })
-    })
+        console.log(townObject);
 
-    heroes.sort((a, b) => a.level - b.level);
-
-    heroes.forEach(hero => {
-        console.log(`Hero: ${hero.name}`);
-        console.log(`level => ${hero.level}`);
-        console.log(`items => ${hero.items.join(', ')}`);
     })
 }
 
-const input1 = [
-    'Isacc / 25 / Apple, GravityGun',
-    'Derek / 12 / BarrelVest, DestructionSword',
-    'Hes / 1 / Desolator, Sentinel, Antara'
-];
+const input1 = ['Sofia | 42.696552 | 23.32601', 'Beijing | 39.913818 | 116.363625'];
+const input2 = ['Plovdiv | 136.45 | 812.575'];
 
-const input2 = [
-    'Batman / 2 / Banana, Gun',
-    'Superman / 18 / Sword',
-    'Poppy / 28 / Sentinel, Antara'
-];
+console.log("Output for input1:");
+processTowns(input1);
 
-registerHeroes(input1);
-registerHeroes(input2);
+console.log("---------------------------------------------");
+
+console.log("Output for input2:");
+processTowns(input2);
