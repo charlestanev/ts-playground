338815a05a6ec41cad86e685d88a6940e55e71b5
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2024-12-19 16:37:19 +0200

    Implement town population registry and product price comparison functions

--- Advanced Data Types Tasks/src/index.ts
+++ Advanced Data Types Tasks/src/index.ts
@@ -154,52 +154,189 @@
 
 
 
-// 4.	Towns
-// You’re tasked to create and print objects from a text table. 
-// You will receive the input as an array of strings, where each string represents a table row, with values on the row separated by pipes " | " and spaces.
-// The table will consist of exactly 3 columns "Town", "Latitude" and "Longitude". The latitude and longitude columns will always contain valid numbers. Check the examples to get a better understanding of your task.
-// The output should be objects. Latitude and longitude must be parsed to numbers and formatted to the second decimal point!
+// // 4.	Towns
+// // You’re tasked to create and print objects from a text table.
+// // You will receive the input as an array of strings, where each string represents a table row, with values on the row separated by pipes " | " and spaces.
+// // The table will consist of exactly 3 columns "Town", "Latitude" and "Longitude". The latitude and longitude columns will always contain valid numbers. Check the examples to get a better understanding of your task.
+// // The output should be objects. Latitude and longitude must be parsed to numbers and formatted to the second decimal point!
+// // Examples
+// // Input
+// // ['Sofia | 42.696552 | 23.32601',
+// // 'Beijing | 39.913818 | 116.363625']
+// // Output
+// // { town: 'Sofia', latitude: '42.70', longitude: '23.33' }
+// // { town: 'Beijing', latitude: '39.91', longitude: '116.36' }
+
+// // Input
+// // ['Plovdiv | 136.45 | 812.575']
+// // Output
+// // { town: 'Plovdiv', latitude: '136.45', longitude: '812.58' }
+
+// interface Town {
+//     town: string;
+//     latitude: string;
+//     longitude: string;
+// }
+
+// function processTowns(input: string[]): void {
+//     input.forEach(row => {
+//         const [town, latitude, longitude] = row.split(' | ');
+
+//         const townObject: Town = {
+//             town: town,
+//             latitude: Number(latitude).toFixed(2),
+//             longitude: Number(longitude).toFixed(2)
+//         }
+
+//         console.log(townObject);
+
+//     })
+// }
+
+// const input1 = ['Sofia | 42.696552 | 23.32601', 'Beijing | 39.913818 | 116.363625'];
+// const input2 = ['Plovdiv | 136.45 | 812.575'];
+
+// console.log("Output for input1:");
+// processTowns(input1);
+
+// console.log("---------------------------------------------");
+
+// console.log("Output for input2:");
+// processTowns(input2);
+
+
+
+
+// // 5.	Town Population
+// // You have been tasked to create a registry for different towns and their population.
+// // Input
+// // The input comes as array of strings. Each element will contain data for a town and its population in the following format: "{townName} <-> {townPopulation}"
+// // If you receive the same town twice, you should add the given population to the current one.
+// // Output
+// // As output, you must print all the towns and their population.
+// // Examples
+// // Input	Output
+// // ['Sofia <-> 1200000',
+// // 'Montana <-> 20000',
+// // 'New York <-> 10000000',
+// // 'Washington <-> 2345000',
+// // 'Las Vegas <-> 1000000']	Sofia : 1200000
+// // Montana : 20000
+// // New York : 10000000
+// // Washington : 2345000
+// // Las Vegas : 1000000
+// // ['Istanbul <-> 100000',
+// // 'Honk Kong <-> 2100004',
+// // 'Jerusalem <-> 2352344',
+// // 'Mexico City <-> 23401925',
+// // 'Istanbul <-> 1000']	Istanbul : 101000
+// // Honk Kong : 2100004
+// // Jerusalem : 2352344
+// // Mexico City : 23401925
+
+// interface TownPopulation {
+//     [townName: string]: number;
+// }
+
+// function registerTownPopulation(input: string[]): void {
+//     const towns: TownPopulation = {};
+
+//     input.forEach(row => {
+//         const [townName, population] = row.split(' <-> ');
+//         const populationNumber = Number(population);
+
+//         if (towns[townName]) {
+//             towns[townName] += populationNumber;
+//         } else {
+//             towns[townName] = populationNumber
+//         }
+//     });
+
+//     for (const i in towns) {
+//         console.log(`${i} : ${towns[i]}`);
+//     }
+// }
+
+// // Примерни входни данни
+// const input1 = [
+//     'Sofia <-> 1200000',
+//     'Montana <-> 20000',
+//     'New York <-> 10000000',
+//     'Washington <-> 2345000',
+//     'Las Vegas <-> 1000000'
+// ];
+
+// const input2 = [
+//     'Istanbul <-> 100000',
+//     'Honk Kong <-> 2100004',
+//     'Jerusalem <-> 2352344',
+//     'Mexico City <-> 23401925',
+//     'Istanbul <-> 1000'
+// ];
+
+// console.log("Output for input1:");
+// registerTownPopulation(input1);
+
+// console.log("\nOutput for input2:");
+// registerTownPopulation(input2);
+
+
+
+
+// 6.	Lowest Prices in Cities
+// You will be given several towns, with products and their price. You need to find the lowest price for every product and the town it is sold at for that price.
+// Input
+// The input comes as an array of strings. Each element will hold data about a town, product, and its price at that town. The town and product will be strings, the price will be a number. The input will come in the following format:
+// {townName} | {productName} | {productPrice}
+// Output
+// As output, you must print each product with its lowest price and the town at which the product is sold at that price. If two towns share the same lowest price, print the one that was entered first. 
+// The output, for every product, should be in the following format:
+// {productName} -> {productLowestPrice} ({townName})
+// The order of output in - order of entrance. See the examples for more info.
 // Examples
-// Input
-// ['Sofia | 42.696552 | 23.32601',
-// 'Beijing | 39.913818 | 116.363625']
-// Output
-// { town: 'Sofia', latitude: '42.70', longitude: '23.33' }
-// { town: 'Beijing', latitude: '39.91', longitude: '116.36' }
+// Input	Output
+// ['Sample Town | Sample Product | 1000',
+// 'Sample Town | Orange | 2',
+// 'Sample Town | Peach | 1',
+// 'Sofia | Orange | 3',
+// 'Sofia | Peach | 2',
+// 'New York | Sample Product | 1000.1',
+// 'New York | Burger | 10']	Sample Product -> 1000 (Sample Town)
+// Orange -> 2 (Sample Town)
+// Peach -> 1 (Sample Town)
+// Burger -> 10 (New York)
 
-// Input
-// ['Plovdiv | 136.45 | 812.575']
-// Output
-// { town: 'Plovdiv', latitude: '136.45', longitude: '812.58' }
-
-interface Town {
+interface ProductInfo {
+    price: number;
     town: string;
-    latitude: string;
-    longitude: string;
 }
 
-function processTowns(input: string[]): void {
+function findLowestPrices(input: string[]): void {
+    const productRegistry: { [productName: string]: ProductInfo } = {}
+
     input.forEach(row => {
-        const [town, latitude, longitude] = row.split(' | ');
+        const [town, product, price] = row.split(' | ');
+        const productPrice = Number(price);
 
-        const townObject: Town = {
-            town: town,
-            latitude: Number(latitude).toFixed(2),
-            longitude: Number(longitude).toFixed(2)
+        if (!productRegistry[product] || productRegistry[product].price > productPrice) {
+            productRegistry[product] = { price: productPrice, town };
         }
+    });
 
-        console.log(townObject);
-
-    })
+    for (const product in productRegistry) {
+        const { price, town } = productRegistry[product];
+        console.log(`${product} -> ${price} (${town})`);
+    }
 }
 
-const input1 = ['Sofia | 42.696552 | 23.32601', 'Beijing | 39.913818 | 116.363625'];
-const input2 = ['Plovdiv | 136.45 | 812.575'];
+const input = [
+    'Sample Town | Sample Product | 1000',
+    'Sample Town | Orange | 2',
+    'Sample Town | Peach | 1',
+    'Sofia | Orange | 3',
+    'Sofia | Peach | 2',
+    'New York | Sample Product | 1000.1',
+    'New York | Burger | 10'
+];
 
-console.log("Output for input1:");
-processTowns(input1);
-
-console.log("---------------------------------------------");
-
-console.log("Output for input2:");
-processTowns(input2);
+findLowestPrices(input);
