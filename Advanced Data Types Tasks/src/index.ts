ead5271bd250d50865d9086500283abbe4c7e12e
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2024-12-19 18:28:10 +0200

    Implement enhancements for town data processing and hero management

--- Advanced Data Types Tasks/src/index.ts
+++ Advanced Data Types Tasks/src/index.ts
@@ -1,128 +1,343 @@
-// // Lab: OOP
-// // 1.	Car Info
-// // Create a class named Car.
-// // The class should have public fields for:
-// // •	Brand: text
-// // •	Model: text
-// // •	Horsepower: integer number
-// // Create getters and setters for each class field.
-// // Test the Program
-// // •	The input consists of line - representing car object.
-// // •	You will have car info in the following format "{brand} {model} {horsePower}" separated by single space.
-// // •	Print car in the following format: "The car is: {brand} {model} – {horsePower} HP."
+// // 1.	Calorie Object
+// // Write a function that composes an object by given properties. The input comes as an array of strings. Every even index of the array represents the name of the food. Every odd index is a number that is equal to the calories in 100 grams of the given product. Assign each value to its corresponding property, and finally print the object.
+// // The input comes as an array of string elements.
+// // The output should be printed on the console.
 // // Examples
 // // Input	Output
-// // Chevrolet Impala 390	The car is: Chevrolet Impala - 390 HP.
-// // Skoda Karoq 150	The car is: This Skoda Karoq - 150 HP.
+// // ['Yoghurt', '48', 'Rise', '138', 'Apple', '52']	{ Yoghurt: 48, Rise: 138, Apple: 52 }
+// // ['Potato', '93', 'Skyr', '63', 'Cucumber', '18', 'Milk', '42']	{ Potato: 93, Skyr: 63, Cucumber: 18, Milk: 42 }
 
-// interface ICar {
-//     brand: string;
-//     model: string;
-//     horsePower: number;
+// interface FoodCalories {
+//     [foodName: string]: number;
 // }
 
-// class Car implements ICar {
-//     private _brand: string;
-//     private _model: string;
-//     private _horsepower: number;
+// function createCalorieObject(input: string[]): FoodCalories {
+//     const result: FoodCalories = {};
 
-//     constructor(brand: string, model: string, horsepower: number) {
-//         this._brand = brand;
-//         this._model = model;
-//         this._horsepower = horsepower;
+//     for (let i = 0; i < input.length; i += 2) {
+//         const food = input[i];
+//         const calories = Number(input[i + 1]);
+//         result[food] = calories;
 //     }
+//     return result;
+// }
 
-//     public get brand(): string {
-//         return this._brand;
-//     }
+// const input1 = ['Yoghurt', '48', 'Rise', '138', 'Apple', '52'];
+// const input2 = ['Potato', '93', 'Skyr', '63', 'Cucumber', '18', 'Milk', '42'];
 
-//     public set brand(value: string) {
-//         this._brand = value;
-//     }
+// console.log(createCalorieObject(input1));
+// console.log(createCalorieObject(input2));
 
-//     public get model(): string {
-//         return this._model;
-//     }
 
-//     public set model(value: string) {
-//         this._model = value;
-//     }
 
-//     public get horsePower(): number {
-//         return this._horsepower;
-//     }
 
-//     public set horsepowers(value: number) {
-//         this._horsepower = value;
-//     }
 
-//     public getCarInfo(): string {
-//         return `The car is: ${this._brand} ${this._model} - ${this._horsepower} HP.`
+// // 2.	Person Info
+// // Write a function that receives 3 parameters, sets them to an object, and returns that object.
+// // The input comes as 3 separate strings in the following order: firstName, lastName, age.
+// // Examples
+// // Input	Object Properties
+// // "Peter",
+// // "Pan",
+// // "20"	firstName: Peter
+// // lastName: Pan
+// // age: 20
+// // "George",
+// // "Smith",
+// // "18"	firstName: George
+// // lastName: Smith
+// // age: 18
+
+// interface PersonInfo {
+//     firstName: string;
+//     lastName: string;
+//     age: number;
+// }
+
+// function createPersonInfo(firstName: string, lastName: string, age: string): PersonInfo {
+//     return {
+//         firstName,
+//         lastName,
+//         age: Number(age)
+//     };
+// }
+// const person1 = createPersonInfo("Peter", "Pan", "20");
+// const person2 = createPersonInfo("George", "Smith", "18");
+
+// console.log(person1);
+// console.log(person2);
+
+
+
+// // 3.	Inventory
+// // Create a function, which creates a register for heroes, with their names, level, and items (if they have such).
+// // The input comes as an array of strings. Each element holds data for a hero, in the following format:
+// // "{heroName} / {heroLevel} / {item1}, {item2}, {item3}..."
+// // You must store the data about every hero. The name is a string, a level is a number and the items are all strings.
+// // The output is all of the data for all the heroes you’ve stored sorted ascending by level. The data must be in the following format for each hero:
+// // Hero: {heroName}
+// // level => {heroLevel}
+// // Items => {item1}, {item2}, {item3}
+// // Examples
+// // Input	Output
+// // [
+// // 'Isacc / 25 / Apple, GravityGun',
+// // 'Derek / 12 / BarrelVest, DestructionSword',
+// // 'Hes / 1 / Desolator, Sentinel, Antara'
+// // ]	Hero: Hes
+// // level => 1
+// // items => Desolator, Sentinel, Antara
+// // Hero: Derek
+// // level => 12
+// // items => BarrelVest, DestructionSword
+// // Hero: Isacc
+// // level => 25
+// // items => Apple, GravityGun
+// // [
+// // 'Batman / 2 / Banana, Gun',
+// // 'Superman / 18 / Sword',
+// // 'Poppy / 28 / Sentinel, Antara'
+// // ]	Hero: Batman
+// // level => 2
+// // items => Banana, Gun
+// // Hero: Superman
+// // level => 18
+// // items => Sword
+// // Hero: Poppy
+// // level => 28
+// // items => Sentinel, Antara
+
+// interface Hero {
+//     name: string;
+//     level: number;
+//     items: string[];
+// }
+
+// function registerHeroes(input: string[]): void {
+//     const heroes: Hero[] = [];
+
+//     input.forEach(x => {
+//         const [name, level, items] = x.split(' / ');
+
+//         heroes.push({
+//             name,
+//             level: Number(level),
+//             items: items ? items.split(', ') : []
+//         })
+//     })
+
+//     heroes.sort((a, b) => a.level - b.level);
+
+//     heroes.forEach(hero => {
+//         console.log(`Hero: ${hero.name}`);
+//         console.log(`level => ${hero.level}`);
+//         console.log(`items => ${hero.items.join(', ')}`);
+//     })
+// }
+
+// const input1 = [
+//     'Isacc / 25 / Apple, GravityGun',
+//     'Derek / 12 / BarrelVest, DestructionSword',
+//     'Hes / 1 / Desolator, Sentinel, Antara'
+// ];
+
+// const input2 = [
+//     'Batman / 2 / Banana, Gun',
+//     'Superman / 18 / Sword',
+//     'Poppy / 28 / Sentinel, Antara'
+// ];
+
+// registerHeroes(input1);
+// registerHeroes(input2);
+
+
+
+
+
+// // 4.	Towns
+// // You’re tasked to create and print objects from a text table.
+// // You will receive the input as an array of strings, where each string represents a table row, with values on the row separated by pipes " | " and spaces.
+// // The table will consist of exactly 3 columns "Town", "Latitude" and "Longitude". The latitude and longitude columns will always contain valid numbers. Check the examples to get a better understanding of your task.
+// // The output should be objects. Latitude and longitude must be parsed to numbers and formatted to the second decimal point!
+// // Examples
+// // Input
+// // ['Sofia | 42.696552 | 23.32601',
+// // 'Beijing | 39.913818 | 116.363625']
+// // Output
+// // { town: 'Sofia', latitude: '42.70', longitude: '23.33' }
+// // { town: 'Beijing', latitude: '39.91', longitude: '116.36' }
+
+// // Input
+// // ['Plovdiv | 136.45 | 812.575']
+// // Output
+// // { town: 'Plovdiv', latitude: '136.45', longitude: '812.58' }
+
+// interface Town {
+//     town: string;
+//     latitude: string;
+//     longitude: string;
+// }
+
+// function processTowns(input: string[]): void {
+//     input.forEach(row => {
+//         const [town, latitude, longitude] = row.split(' | ');
+
+//         const townObject: Town = {
+//             town: town,
+//             latitude: Number(latitude).toFixed(2),
+//             longitude: Number(longitude).toFixed(2)
+//         }
+
+//         console.log(townObject);
+
+//     })
+// }
+
+// const input1 = ['Sofia | 42.696552 | 23.32601', 'Beijing | 39.913818 | 116.363625'];
+// const input2 = ['Plovdiv | 136.45 | 812.575'];
+
+// console.log("Output for input1:");
+// processTowns(input1);
+
+// console.log("---------------------------------------------");
+
+// console.log("Output for input2:");
+// processTowns(input2);
+
+
+
+
+// // 5.	Town Population
+// // You have been tasked to create a registry for different towns and their population.
+// // Input
+// // The input comes as array of strings. Each element will contain data for a town and its population in the following format: "{townName} <-> {townPopulation}"
+// // If you receive the same town twice, you should add the given population to the current one.
+// // Output
+// // As output, you must print all the towns and their population.
+// // Examples
+// // Input	Output
+// // ['Sofia <-> 1200000',
+// // 'Montana <-> 20000',
+// // 'New York <-> 10000000',
+// // 'Washington <-> 2345000',
+// // 'Las Vegas <-> 1000000']	Sofia : 1200000
+// // Montana : 20000
+// // New York : 10000000
+// // Washington : 2345000
+// // Las Vegas : 1000000
+// // ['Istanbul <-> 100000',
+// // 'Honk Kong <-> 2100004',
+// // 'Jerusalem <-> 2352344',
+// // 'Mexico City <-> 23401925',
+// // 'Istanbul <-> 1000']	Istanbul : 101000
+// // Honk Kong : 2100004
+// // Jerusalem : 2352344
+// // Mexico City : 23401925
+
+// interface TownPopulation {
+//     [townName: string]: number;
+// }
+
+// function registerTownPopulation(input: string[]): void {
+//     const towns: TownPopulation = {};
+
+//     input.forEach(row => {
+//         const [townName, population] = row.split(' <-> ');
+//         const populationNumber = Number(population);
+
+//         if (towns[townName]) {
+//             towns[townName] += populationNumber;
+//         } else {
+//             towns[townName] = populationNumber
+//         }
+//     });
+
+//     for (const i in towns) {
+//         console.log(`${i} : ${towns[i]}`);
 //     }
 // }
 
-// function processCarInput(input: string): void {
-//     const [brand, model, horsepowerStr] = input.split(' ');
-//     const horsepower = parseInt(horsepowerStr, 10);
+// // Примерни входни данни
+// const input1 = [
+//     'Sofia <-> 1200000',
+//     'Montana <-> 20000',
+//     'New York <-> 10000000',
+//     'Washington <-> 2345000',
+//     'Las Vegas <-> 1000000'
+// ];
 
-//     if (!brand || !model || isNaN(horsepower)) {
-//         throw new Error("Невалиден формат на входа. Очакван формат: '{марка} {модел} {конски сили}'.");
-//     }
+// const input2 = [
+//     'Istanbul <-> 100000',
+//     'Honk Kong <-> 2100004',
+//     'Jerusalem <-> 2352344',
+//     'Mexico City <-> 23401925',
+//     'Istanbul <-> 1000'
+// ];
 
-//     const car = new Car(brand, model, horsepower);
-//     console.log(car.getCarInfo());
-// }
+// console.log("Output for input1:");
+// registerTownPopulation(input1);
 
-// const inputs = ["Chevrolet Impala 390", "Skoda Karoq 150"];
-// inputs.forEach((input) => {
-//     try {
-//         processCarInput(input);
-//     } catch (error) {
-//         console.error(error.message);
-//     }
-// });
+// console.log("\nOutput for input2:");
+// registerTownPopulation(input2);
 
 
 
-// 2.	Opinion Poll
-// •	Create a Person class with two fields name and age.
-// •	Write a program that uses line of personal information in the following format "{name} {age}"
-// •	Print the data in the following format: "{name} is {age} years old."
+
+// 6.	Lowest Prices in Cities
+// You will be given several towns, with products and their price. You need to find the lowest price for every product and the town it is sold at for that price.
+// Input
+// The input comes as an array of strings. Each element will hold data about a town, product, and its price at that town. The town and product will be strings, the price will be a number. The input will come in the following format:
+// {townName} | {productName} | {productPrice}
+// Output
+// As output, you must print each product with its lowest price and the town at which the product is sold at that price. If two towns share the same lowest price, print the one that was entered first. 
+// The output, for every product, should be in the following format:
+// {productName} -> {productLowestPrice} ({townName})
+// The order of output in - order of entrance. See the examples for more info.
 // Examples
 // Input	Output
-// Peter 12	Peter is 12 years old.
-// Sofia 33	Sofia is 33 years old.
-// interface IPoll {
-//     name: string,
-//     age: number
-// }
+// ['Sample Town | Sample Product | 1000',
+// 'Sample Town | Orange | 2',
+// 'Sample Town | Peach | 1',
+// 'Sofia | Orange | 3',
+// 'Sofia | Peach | 2',
+// 'New York | Sample Product | 1000.1',
+// 'New York | Burger | 10']	Sample Product -> 1000 (Sample Town)
+// Orange -> 2 (Sample Town)
+// Peach -> 1 (Sample Town)
+// Burger -> 10 (New York)
 
-class Person {
-    name: string;
-    age: number;
+interface ProductInfo {
+    price: number;
+    town: string;
+}
 
-    constructor(name: string, age: number) {
-        this.name = name;
-        this.age = age;
-    }
+function findLowestPrices(input: string[]): void {
+    const productRegistry: { [productName: string]: ProductInfo } = {}
 
-    getInfo(): string {
-        return `${this.name} is ${this.age} years old.`;
+    input.forEach(row => {
+        const [town, product, price] = row.split(' | ');
+        const productPrice = Number(price);
+
+        if (!productRegistry[product] || productRegistry[product].price > productPrice) {
+            productRegistry[product] = { price: productPrice, town };
+        }
+    });
+
+    for (const product in productRegistry) {
+        const { price, town } = productRegistry[product];
+        console.log(`${product} -> ${price} (${town})`);
     }
 }
 
-function processOpinionPoll(input: string[]): void {
-    input.forEach(data => {
-        const [name, ageStr] = data.split(' ');
+const input = [
+    'Sample Town | Sample Product | 1000',
+    'Sample Town | Orange | 2',
+    'Sample Town | Peach | 1',
+    'Sofia | Orange | 3',
+    'Sofia | Peach | 2',
+    'New York | Sample Product | 1000.1',
+    'New York | Burger | 10'
+];
 
-        const age = Number(ageStr);
+findLowestPrices(input);
 
-        const person = new Person(name, age);
-
-        console.log(person.getInfo());
-    })
-}
-
-const inputs = ['Peter 12', 'Sofia 33'];
-
-processOpinionPoll(inputs)
