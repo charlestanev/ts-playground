405d84afcbff1eddc3b7473bf6b3a77e6ecce559
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2024-12-23 15:20:45 +0200

    Implement CarDealership class with generic interfaces for managing sold cars

--- Generics exercises/src/index.ts
+++ Generics exercises/src/index.ts
@@ -71,3 +71,85 @@
 
 // let c = new CompareElements([1, 2, 3, 4, 5, 1, 1]);
 // console.log(c.compare(1));
+
+
+// 3.	Car Dealership
+// Create two generic interfaces:
+// •	Dealership that takes one generic type parameter: dealershipName and another property - soldCars (number)
+// •	Actions that takes one generic type parameter and defines a sellCar(dealerID , model) method, where both parameters are of the generic type passed to the interface
+
+// Create a class CarDealership that implements both Dealership and Actions and has one property of its own:
+// •	Public property modelsSold which is initially set to empty object
+// Note: The dealershipName is taken through the constructor and the soldCars is initially set to 0.
+// Make the following two methods:
+// •	sellCar(dealerID , model)  – add the dealerID as a key and the model as value in the modelsSold object and increase the soldCars by 1;
+// •	showDetails() – return a message in the following format:
+
+// "{Dealership}:
+// {dealerID} sold {model}
+// ..."
+// Examples
+// Input	Output
+// let dealership = new CarDealer('SilverStar');
+
+// dealership.sellCar('BG01', 'C Class');
+// dealership.sellCar('BG02', 'S Class');
+// dealership.sellCar('BG03', 'ML Class');
+// dealership.sellCar('BG04', 'CLK Class');
+// console.log(dealership.showDetails());	SilverStar:
+// BG01 sold C Class
+// BG02 sold S Class
+// BG03 sold ML Class
+// BG04 sold CLK Class
+
+interface Dealership<T> {
+    dealershipName: T;
+    soldCars: number;
+}
+
+interface Actions<T> {
+    sellCar(dealerID: T, model: T): void;
+}
+
+class CarDealership implements Dealership<string>, Actions<string> {
+    public dealershipName: string;
+    public soldCars: number;
+    public modelsSold: { [dealerID: string]: string };
+
+    constructor(dealershipName: string) {
+        this.dealershipName = dealershipName;
+        this.soldCars = 0;
+        this.modelsSold = {};
+    }
+
+    public sellCar(dealerID: string, model: string): void {
+        this.modelsSold[dealerID] = model;
+        this.soldCars++;
+    }
+
+    public showDetails(): string {
+        let details = `${this.dealershipName}:`;
+
+        for (const dealerID in this.modelsSold) {
+            details += ` ${dealerID} sold ${this.modelsSold[dealerID]}`;
+            if (Object.keys(this.modelsSold).indexOf(dealerID) < Object.keys(this.modelsSold).length - 1) {
+                details += `,`;
+            }
+        }
+
+        return details;
+    }
+
+    public logSoldCars(): void {
+        console.log(`Total sold cars: ${this.soldCars}`);
+    }
+}
+
+const dealership = new CarDealership('SilverStar');
+
+dealership.sellCar('BG01', 'C Class');
+dealership.sellCar('BG02', 'S Class');
+dealership.sellCar('BG03', 'ML Class');
+dealership.sellCar('BG04', 'CLK Class');
+
+console.log(dealership.showDetails());
