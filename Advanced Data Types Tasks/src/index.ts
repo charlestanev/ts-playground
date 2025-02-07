ff48fb14d79e7d0cffc4799debd97dda6d9712f4
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2025-02-07 12:43:56 +0200

    Implement TownParser class to parse and format town data from input strings

--- Advanced Data Types Tasks/src/index.ts
+++ Advanced Data Types Tasks/src/index.ts
@@ -146,3 +146,51 @@
 
 // const heroesRegistry = createHeroRegistry(heroesData);
 // printHeroes(heroesRegistry);
+
+
+
+
+// // 4.	Towns
+// // You’re tasked to create and print objects from a text table.
+// // You will receive the input as an array of strings, where each string represents a table row, with values on the row separated by pipes " | " and spaces.
+// // The table will consist of exactly 3 columns "Town", "Latitude" and "Longitude". The latitude and longitude columns will always contain valid numbers. Check the examples to get a better understanding of your task.
+// // The output should be objects. Latitude and longitude must be parsed to numbers and formatted to the second decimal point!
+// // Examples
+// // Input
+// // ['Sofia | 42.696552 | 23.32601',
+// // 'Beijing | 39.913818 | 116.363625']
+// // Output
+// // { town: 'Sofia', latitude: '42.70', longitude: '23.33' }
+// // { town: 'Beijing', latitude: '39.91', longitude: '116.36' }
+
+// // Input
+// // ['Plovdiv | 136.45 | 812.575']
+// // Output
+// // { town: 'Plovdiv', latitude: '136.45', longitude: '812.58' }
+
+// interface Town {
+//     town: string;
+//     latitude: string;
+//     longitute: string;
+// }
+
+// class TownParser {
+//     static parse(input: string[]): Town[] {
+//         return input.map(row => {
+//             const [town, latitude, longitude] = row.split(' | ');
+
+//             const formatedLatitude = Number(latitude).toFixed(2);
+//             const formatedLongitude = Number(longitude).toFixed(2);
+//             return { town, latitude: formatedLatitude, longitute: formatedLongitude }
+//         })
+//     }
+// }
+
+// const input = [
+//     'Sofia | 42.696552 | 23.32601',
+//     'Beijing | 39.913818 | 116.363625'
+// ];
+
+// const towns = TownParser.parse(input);
+
+// towns.forEach(town => console.log(town));
