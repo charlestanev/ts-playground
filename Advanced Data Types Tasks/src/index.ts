ae6e2e3403f366aef08fd63ad401bc80f3a65842
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2025-02-07 12:54:32 +0200

    Add function to process town population data from input strings

--- Advanced Data Types Tasks/src/index.ts
+++ Advanced Data Types Tasks/src/index.ts
@@ -194,3 +194,74 @@
 // const towns = TownParser.parse(input);
 
 // towns.forEach(town => console.log(town));
+
+
+
+
+
+// // 5.	Town Population
+// // You have been tasked to create a registry for different towns and their population.
+// // Input
+// // The input comes as array of strings. Each element will contain data for a town and its population in the following format: "{townName} <-> {townPopulation}"
+// // If you receive the same town twice, you should add the given population to the current one.
+// // Output
+// // As output, you must print all the towns and their population.
+// // Examples
+// // Input	Output
+// // ['Sofia <-> 1200000',
+// // 'Montana <-> 20000',
+// // 'New York <-> 10000000',
+// // 'Washington <-> 2345000',
+// // 'Las Vegas <-> 1000000']	Sofia : 1200000
+// // Montana : 20000
+// // New York : 10000000
+// // Washington : 2345000
+// // Las Vegas : 1000000
+// // ['Istanbul <-> 100000',
+// // 'Honk Kong <-> 2100004',
+// // 'Jerusalem <-> 2352344',
+// // 'Mexico City <-> 23401925',
+// // 'Istanbul <-> 1000']	Istanbul : 101000
+// // Honk Kong : 2100004
+// // Jerusalem : 2352344
+// // Mexico City : 23401925
+
+// function processTowns(input: string[]): void {
+//     const towns: {
+//         [key: string]: number
+//     } = {}
+
+//     for (const townData of input) {
+//         const [name, population] = townData.split(' <-> ');
+//         const populationNumber = Number(population);
+//         if (towns[name]) {
+//             towns[name] += populationNumber
+//         } else {
+//             towns[name] = populationNumber
+//         }
+//     }
+
+//     for (const town in towns) {
+//         console.log(`${town} : ${towns[town]}`);
+//     }
+// }
+
+// const input1 = [
+//     'Sofia <-> 1200000',
+//     'Montana <-> 20000',
+//     'New York <-> 10000000',
+//     'Washington <-> 2345000',
+//     'Las Vegas <-> 1000000'
+// ];
+
+// const input2 = [
+//     'Istanbul <-> 100000',
+//     'Honk Kong <-> 2100004',
+//     'Jerusalem <-> 2352344',
+//     'Mexico City <-> 23401925',
+//     'Istanbul <-> 1000'
+// ];
+
+// processTowns(input1);
+// console.log('----------------');
+// processTowns(input2);
