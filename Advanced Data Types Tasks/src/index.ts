b23930778d1aaf1751f1921f96c861e12bdc11fb
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2025-02-07 13:03:38 +0200

    Add function to find lowest prices for products in towns with example input and output

--- Advanced Data Types Tasks/src/index.ts
+++ Advanced Data Types Tasks/src/index.ts
@@ -265,3 +265,62 @@
 // processTowns(input1);
 // console.log('----------------');
 // processTowns(input2);
+
+
+
+
+
+// // 6.	Lowest Prices in Cities
+// // You will be given several towns, with products and their price. You need to find the lowest price for every product and the town it is sold at for that price.
+// // Input
+// // The input comes as an array of strings. Each element will hold data about a town, product, and its price at that town. The town and product will be strings, the price will be a number. The input will come in the following format:
+// // {townName} | {productName} | {productPrice}
+// // Output
+// // As output, you must print each product with its lowest price and the town at which the product is sold at that price. If two towns share the same lowest price, print the one that was entered first.
+// // The output, for every product, should be in the following format:
+// // {productName} -> {productLowestPrice} ({townName})
+// // The order of output in - order of entrance. See the examples for more info.
+// // Examples
+// // Input	Output
+// // ['Sample Town | Sample Product | 1000',
+// // 'Sample Town | Orange | 2',
+// // 'Sample Town | Peach | 1',
+// // 'Sofia | Orange | 3',
+// // 'Sofia | Peach | 2',
+// // 'New York | Sample Product | 1000.1',
+// // 'New York | Burger | 10']	Sample Product -> 1000 (Sample Town)
+// // Orange -> 2 (Sample Town)
+// // Peach -> 1 (Sample Town)
+// // Burger -> 10 (New York)
+
+// function findLowestPrices(input: string[]): void {
+//     const products: {
+//         [product: string]: { price: number, town: string }
+//     } = {}
+
+//     for (const entry of input) {
+//         const [town, product, priceString] = entry.split(' | ');
+//         const price = Number(priceString);
+
+//         if (!products[product] || price < products[product].price) {
+//             products[product] = { price, town }
+//         }
+//     }
+
+//     for (const product in products) {
+//         console.log(`${product} -> ${products[product].price} (${products[product].town})`);
+//     }
+
+// }
+
+// const input1 = [
+//     'Sample Town | Sample Product | 1000',
+//     'Sample Town | Orange | 2',
+//     'Sample Town | Peach | 1',
+//     'Sofia | Orange | 3',
+//     'Sofia | Peach | 2',
+//     'New York | Sample Product | 1000.1',
+//     'New York | Burger | 10'
+// ];
+
+// findLowestPrices(input1);
