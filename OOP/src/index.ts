9b8617708715b3ad31c1a87aa9336c802e798929
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2025-02-08 15:54:39 +0200

    Add Person class and input processing for personal information

--- OOP/src/index.ts
+++ OOP/src/index.ts
@@ -4,10 +4,10 @@
 // // •	Brand: text
 // // •	Model: text
 // // •	Horsepower: integer number
-// // Create getters and setters for each class field. 
-// // Test the Program 
-// // •	The input consists of line - representing car object. 
-// // •	You will have car info in the following format "{brand} {model} {horsePower}" separated by single space. 
+// // Create getters and setters for each class field.
+// // Test the Program
+// // •	The input consists of line - representing car object.
+// // •	You will have car info in the following format "{brand} {model} {horsePower}" separated by single space.
 // // •	Print car in the following format: "The car is: {brand} {model} – {horsePower} HP."
 // // Examples
 // // Input	Output
@@ -72,3 +72,64 @@
 // const input2 = "Skoda Karoq 150";
 // const car2 = createCarFromInput(input2);
 // console.log(car2.getCarInfo());
+
+
+
+
+
+// // 2.	Opinion Poll
+// // •	Create a Person class with two fields name and age.
+// // •	Write a program that uses line of personal information in the following format "{name} {age}"
+// // •	Print the data in the following format: "{name} is {age} years old."
+// // Examples
+// // Input	Output
+// // Peter 12	Peter is 12 years old.
+// // Sofia 33	Sofia is 33 years old.
+
+// class Person {
+//     private _name: string;
+//     private _age: number;
+
+//     constructor(name: string, age: number) {
+//         this._name = name;
+//         this._age = age;
+//     }
+
+//     get name(): string {
+//         return this._name;
+//     }
+
+//     set name(value: string) {
+//         this._name = value;
+//     }
+
+//     get age(): number {
+//         return this._age;
+//     }
+
+//     set age(value: number) {
+//         value > 0
+//             ? this._age = value
+//             : console.error("Age must be a positive number.");
+//     }
+
+//     getPersonInfo(): string {
+//         return `${this._name} is ${this._age} years old.`
+//     }
+// }
+
+// function createPersonFromInput(input: string): Person {
+//     const [name, ageStr] = input.split(' ');
+//     const age = parseInt(ageStr);
+
+//     return new Person(name, age);
+// }
+
+// const input1 = 'Peter 12';
+// const person1 = createPersonFromInput(input1);
+// console.log(person1.getPersonInfo());
+
+// const input2 = 'Charles 32';
+// const person2 = createPersonFromInput(input2);
+// console.log(person2.getPersonInfo());
+
