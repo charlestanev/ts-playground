d9d39f30b2203c5379e2fe2b1ef8742d3180077f
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2025-02-15 15:19:09 +0200

    Add method logging decorator and update TypeScript configuration for decorators

--- Decorators/src/index.ts
+++ Decorators/src/index.ts
@@ -12,3 +12,37 @@
 //     constructor(public name: string) { }
 // }
 // const user1 = new User('Ivan')
+
+
+
+
+
+// Задача 2
+// Условие:
+// Създайте декоратор, който при извикването на метод извежда съобщения
+// "Започна изпълнението на метод X" и "Завърши изпълнението на метод X".
+
+function logMethod(target: any, methodName: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = function (...args: any[]) {
+        console.log(`Започва изпълнението на метода "${methodName}"`);
+        const result = originalMethod.apply(this, args);
+        console.log(`Завърши методът: ${methodName}`);
+        return result;
+    }
+
+    return descriptor;
+}
+
+class Calculator {
+
+    @logMethod
+
+    add(a: number, b: number): number {
+        return a + b
+    }
+}
+
+const calc = new Calculator();
+calc.add(5, 3); 
