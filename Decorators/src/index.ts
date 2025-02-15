7e657b5416794f7206ca9edb7e83f69d6a2d99e6
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2025-02-15 14:26:37 +0200

    Add commented-out class decorator example and update TypeScript configuration

--- Decorators/src/index.ts
+++ Decorators/src/index.ts
@@ -1,1 +1,14 @@
-console.log('wtf');
+// // Задача 1
+// // Създайте декоратор, който при създаването на обект от даден
+// // клас извежда съобщение в конзолата.
+
+// function logClassCreation(constructor: Function) {
+//     console.log(`Класът ${constructor.name} беше създаден.`);
+// }
+
+// @logClassCreation
+
+// class User {
+//     constructor(public name: string) { }
+// }
+// const user1 = new User('Ivan')
