0ff870fb43c51442c0e9ffdd9a5302606acdbfa0
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2025-02-15 15:40:36 +0200

    Add commented-out examples for class decorator and method logging decorator

--- Decorators/src/index.ts
+++ Decorators/src/index.ts
@@ -17,32 +17,56 @@
 
 
 
-// Задача 2
+
+// // Задача 2 Задача 2: Създайте класов декоратор,
+// // който добавя ново статично свойство към класа.
+
+// function addStaticProperty(constructor: Function) {
+//     (constructor as any).category = 'General';
+//     console.log(`Добавено е статично свойство 'category' със стойност 'General'.`);
+// }
+
+// @addStaticProperty
+
+// class Product {
+//     constructor(public name: string) {
+
+//     }
+// }
+
+// console.log((Product as any).category);
+
+
+
+
+
+
+// Задача 10
 // Условие:
 // Създайте декоратор, който при извикването на метод извежда съобщения
 // "Започна изпълнението на метод X" и "Завърши изпълнението на метод X".
 
-function logMethod(target: any, methodName: string, descriptor: PropertyDescriptor) {
-    const originalMethod = descriptor.value;
+// function logMethod(target: any, methodName: string, descriptor: PropertyDescriptor) {
+//     const originalMethod = descriptor.value;
 
-    descriptor.value = function (...args: any[]) {
-        console.log(`Започва изпълнението на метода "${methodName}"`);
-        const result = originalMethod.apply(this, args);
-        console.log(`Завърши методът: ${methodName}`);
-        return result;
-    }
+//     descriptor.value = function (...args: any[]) {
+//         console.log(`Започва изпълнението на метода "${methodName}"`);
+//         const result = originalMethod.apply(this, args);
+//         console.log(`Завърши методът: ${methodName}`);
+//         return result;
+//     }
 
-    return descriptor;
-}
+//     return descriptor;
+// }
 
-class Calculator {
+// class Calculator {
 
-    @logMethod
+//     @logMethod
 
-    add(a: number, b: number): number {
-        return a + b
-    }
-}
+//     add(a: number, b: number): number {
+//         return a + b
+//     }
+// }
 
-const calc = new Calculator();
-calc.add(5, 3); 
+// const calc = new Calculator();
+// calc.add(5, 3); 
