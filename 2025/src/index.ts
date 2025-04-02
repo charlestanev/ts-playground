977510d08259d4c01d32f2156f7b8e9440f409f4
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2025-04-02 18:10:06 +0300

    Comment out unused functions in index.ts for clarity and maintainability

--- 2025/src/index.ts
+++ 2025/src/index.ts
@@ -18,11 +18,100 @@
 // console.log(solve(5.80));
 
 
-let one = function (n: number): void {
-    while (n >= 1) {
-        console.log(n);
-        n--
-    }
-}
+// let one = function (n: number): void {
+//     while (n >= 1) {
+//         console.log(n);
+//         n--
+//     }
+// }
 
-one(10)
+// one(10)
+
+// function wtf(name: string, age: number, grade: number): void {
+//     console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`);
+// }
+
+// wtf('John', 15, 5.54678)
+
+// function monthPrinter(month: number): string {
+//     if (month > 0 && month < 13) {
+//         switch (month) {
+//             case 1:
+//                 return ('January')
+//             case 2:
+//                 return ('February')
+//             case 3:
+//                 return ('March')
+
+//             default:
+//                 return ('Error!')
+//         }
+//     }
+// }
+// console.log(monthPrinter(2));
+// console.log(monthPrinter(2000));
+
+// function theatrePromotions(day: string, age: number): string {
+//     if (age < 0 || age > 122) {
+//         return "Error !"
+//     }
+
+//     let price: string = '';
+
+//     switch (day) {
+//         case 'Weekday':
+//             if (age <= 18) {
+//                 price = '12$'
+//             } else if (age <= 64) {
+//                 price = '18$'
+//             } else {
+//                 price = '12$'
+//             }
+
+//             break;
+
+//         case 'Weekend':
+//             if (age <= 64) {
+//                 price = '15$'
+//             } else if (age <= 64) {
+//                 price = '20$'
+//             } else {
+//                 price = '15$'
+//             }
+
+//         case 'Holiday':
+//             if (age <= 64) {
+//                 price = '5$'
+//             } else if (age <= 64) {
+//                 price = '12$'
+//             } else {
+//                 price = '10$'
+//             }
+
+//             break;
+
+//         default:
+//             return 'Error !';
+//     }
+//     return price;
+// }
+
+// console.log(theatrePromotions('Weekday', 42));
+// console.log(theatrePromotions('Holiday', -12));
+// console.log(theatrePromotions('Holiday', 15));
+
+// function oddNumbers(num: number): number {
+//     let currentOdd: number = 1;
+//     let sum: number = 0;
+
+//     for (let i = 0; i < num; i++) {
+//         console.log(currentOdd);
+
+//         sum += currentOdd;
+//         currentOdd += 2
+//     }
+//     return sum
+// }
+
+// console.log(oddNumbers(5));
+
