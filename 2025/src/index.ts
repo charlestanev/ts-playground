a2f98d5a14dd2b1519d075bd02a7d4ff80bcd89b
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2025-04-02 12:09:43 +0300

    Refactor index.ts to comment out unused sum function and add solve function for doubling a number

--- 2025/src/index.ts
+++ 2025/src/index.ts
@@ -1,1 +1,10 @@
-console.log('kuromoji');
+// let sum = function (num: number): number {
+//     return num * 4
+// }
+
+// console.log(sum(4));
+
+let solve = (num: number): number => {
+    return (num * 2)
+}
+solve(3)
