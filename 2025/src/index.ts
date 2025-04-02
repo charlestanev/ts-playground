a5ae71552042c5446e4e7ca3a7d3e3e83903f0d7
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2025-04-02 12:32:13 +0300

    Refactor index.ts to comment out unused solve function examples and add one function for countdown logging

--- 2025/src/index.ts
+++ 2025/src/index.ts
@@ -4,7 +4,25 @@
 
 // console.log(sum(4));
 
-let solve = (num: number): number => {
-    return (num * 2)
+// let solve = (num: number): number => {
+//     return (num * 2)
+// }
+// solve(3)
+
+// let solve = function (num: number): string {
+//     return (num >= 5.49 ? 'da' : 'ne')
+// }
+
+// console.log(solve(5.48));
+// console.log(solve(5.50));
+// console.log(solve(5.80));
+
+
+let one = function (n: number): void {
+    while (n >= 1) {
+        console.log(n);
+        n--
+    }
 }
-solve(3)
+
+one(10)
