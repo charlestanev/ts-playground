de476baf89e92fdfeb6680872cd3f043b4d7cb30
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2025-06-08 16:45:35 +0300

    add
    add

--- 2025/src/index.ts
+++ 2025/src/index.ts
@@ -2476,3 +2476,69 @@
 
 // }
 
+
+
+class Song {
+    type: string;
+    name: string;
+    time: string;
+
+    constructor(type: string, name: string, time: string) {
+        this.type = type;
+        this.name = name;
+        this.time = time;
+    }
+}
+
+function songs(input: string[]): void {
+    const numberOfSongs = Number(input.shift());
+
+    const typeToPrint = input.pop();
+
+    const songs: Song[] = [];
+
+    for (let i = 0; i < numberOfSongs; i++) {
+        const [type, name, time] = input[i].split('_');
+
+        const song = new Song(type, name, time);
+
+        songs.push(song);
+
+        console.log('song:' + song);
+
+    }
+
+    console.log('songs' + songs);
+
+    // typeToPrint === 'all'
+    //     ? songs.forEach(song => console.log(song.name)
+    //     : songs
+    // )
+
+
+
+    console.log();
+
+}
+
+songs([
+    '3',
+    'favourite_DownTown_3:14',
+    'favourite_Kiss_4:16',
+    'favourite_Smooth Criminal_4:01',
+    'favourite'
+]);
+
+songs(['4',
+    'favourite_DownTown_3:14',
+    'listenLater_Andalouse_3:24',
+    'favourite_In To The Night_3:58',
+    'favourite_Live It Up_3:48',
+    'listenLater']
+);
+
+songs(['2',
+    'like_Replay_3:15',
+    'ban_Photoshop_3:48',
+    'all']
+);
