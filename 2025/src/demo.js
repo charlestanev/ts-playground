dc7bd60216e6c13004f6a75d9bff336147473117
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2025-06-09 18:57:42 +0300

    kur

--- 2025/src/demo.js
+++ 2025/src/demo.js
@@ -109,3 +109,5 @@ solve(
     ['Chips', '5', 'CocaCola', '9', 'Bananas', '14', 'Pasta', '4', 'Beer', '2'], 
     ['Flour', '44', 'Oil', '12', 'Pasta', '7', 'Tomatoes', '70', 'Bananas', '30']
 )
+
+// https://www.youtube.com/watch?v=QPBTkLsK1t8&t=3s
