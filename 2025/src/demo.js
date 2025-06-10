1a16e41dee48f0d1e459a2fef31e211944d5b5a2
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2025-06-10 10:13:26 +0300

    Enhance stock and delivery management logic in solve function

--- 2025/src/demo.js
+++ 2025/src/demo.js
@@ -100,14 +100,31 @@ function solve(stock, kur) {
 
         delivery[item] = itemCount
     }
+
+    for (const i in shop) {
+        let product = i;
+        let productQuantity = shop[i];
+
+        for (const j in delivery) {
+            let newProduct = j;
+            let newProductQuantity = delivery[j];
+
+            if (newProduct === product) {
+                shop[productQuantity] += newProductQuantity
+            } else {
+                product = newProduct;
+                product[productQuantity] = newProductQuantity
+            }
+
+        }
+    }
+
     console.log(shop);
-    console.log(delivery);
-    
+    // console.log(delivery);
+
 }
 
 solve(
-    ['Chips', '5', 'CocaCola', '9', 'Bananas', '14', 'Pasta', '4', 'Beer', '2'], 
+    ['Chips', '5', 'CocaCola', '9', 'Bananas', '14', 'Pasta', '4', 'Beer', '2'],
     ['Flour', '44', 'Oil', '12', 'Pasta', '7', 'Tomatoes', '70', 'Bananas', '30']
 )
-
-// https://www.youtube.com/watch?v=QPBTkLsK1t8&t=3s
